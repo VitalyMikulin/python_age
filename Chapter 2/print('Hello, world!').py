a3b8eb4d46e973864a4b9fac16bd16cfef0cb327
Malkovich <malkovich@test.ru>
2024-06-24 22:07:12 +0400

    input

--- Chapter 2/print('Hello, world!').py
+++ Chapter 2/print('Hello, world!').py
@@ -1,5 +1,20 @@
-print('Hello, world!')
-message = 'Hello, world!'
-print(message)
-message = 'Hello, Python world!'
-print(message)
+# print('Hello, world!')
+# message = 'Hello, world!'
+# print(message)
+# message = 'Hello, Python world!'
+# print(message)
+# print('Python - лучше "всех!"') #Пуривейт
+
+# a = 45
+# b = 12
+# print("My height is",a)
+
+# roubles = 45
+# print('У меня на счёте', roubles, 'рублей')
+# roubles = 80
+# # print('А теперь там', roubles, 'рублей')
+# a = 'Jessica'
+# b = 'Melissa'
+# # print(a)
+# val = 99
+# print('значение равняется', 'val')

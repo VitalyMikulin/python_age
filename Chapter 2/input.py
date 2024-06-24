dd697ee0513e967e200f96a5a09135ef42024355
Malkovich <malkovich@test.ru>
2024-06-24 23:45:06 +0400

    input2

---
+++ Chapter 2/input.py
@@ -0,0 +1,26 @@
+# name = input('Как вас зовут?\n')
+# # print('Hi,', name)
+# firstname = input('Введите ваше имя: ')
+# lastname = input('Введите вашу фамилию: ')
+# print('Привет,', firstname, lastname,'!')
+
+# hours = input('Сколько вы отработали? ')
+# hours = float(hours)
+# print(type(hours))
+# print(hours)
+
+# hours = float(input('Сколько вы отработали? '))
+# # int(hours)
+# print(hours)
+# print(type(hours))
+# print(hours + 5)
+
+name = input('Как вас зовут? ')
+age = int(input('Сколько вам лет? '))
+salary = float(input('Сколько вы зарабатываете? '))
+
+print('Вот, что вы ввели:')
+print('Имя:', name)
+print('Возраст:', age)
+print('Доход:', salary)
+

7ee9d7e61d87be6dc05eea110949390706b1961e
Malkovich <malkovich@test.ru>
2024-08-19 22:15:10 +0400

    45345

---
+++ Chapter 2/concate.py
@@ -0,0 +1,9 @@
+# firstname = input('Введите ваше имя: ')
+# lastname = input('Введите вашу фамилию: ')
+# fullname = firstname + ' ' + lastname
+# print('Ваше полное имя:', fullname)
+
+print('Введите ' + 
+      'объём ' + 
+      'продаж в день')
+

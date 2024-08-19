e3faa297b8e79abd3c6a19e7fde7bc221b830b81
Malkovich <malkovich@test.ru>
2024-08-19 22:15:57 +0400

    4234

--- Chapter 2/concate.py
+++ Chapter 2/concate.py
@@ -3,7 +3,25 @@
 # fullname = firstname + ' ' + lastname
 # print('Ваше полное имя:', fullname)
 
-print('Введите ' + 
-      'объём ' + 
-      'продаж в день')
+# print('Введите ' + 
+#       'объём ' + 
+#       'продаж в день')
 
+# print('Один', 'Два', 'Три', ' ', sep=' жопа ')
+
+# print('\"Один\"', end=' ')
+# print('Два', end=' ')
+# print('Три')
+
+# name = 'john'
+# print('привет, {name}')
+
+# val = 10
+# print(f'Значение равно {val + 7}')
+
+# amount_due = 5000
+# monthly_payment = amount_due / 12.0
+# print(f'Ежемесячный платёж составляет {monthly_payment:.2f}')
+
+number = 1234567890.123456
+print(f'{number:,.2f}')

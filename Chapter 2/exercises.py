26c7ebadd2f77ad1a4a0358fc9e737350fee2c63
Malkovich <malkovich@test.ru>
2024-09-02 21:52:08 +0400

    4

--- Chapter 2/exercises.py
+++ Chapter 2/exercises.py
@@ -6,3 +6,18 @@
 
 # №3
 
+# square = float(input('Введите общую площадь земли в кв. метрах: '))
+# print(f'Площадь земли в акрах равна: {square / 4047}')
+
+# №4
+# purchase1 = float(input('Введите стоимость товара 1: '))
+# purchase2 = float(input('Введите стоимость товара 2: '))
+# purchase3 = float(input('Введите стоимость товара 3: '))
+# purchase4 = float(input('Введите стоимость товара 4: '))
+# purchase5 = float(input('Введите стоимость товара 5: '))
+# p_summ = purchase1 + purchase2 + purchase3 + purchase4 + purchase5
+# tax = p_summ * 0.07
+# total_summ = tax + p_summ
+# print(f'Сумма всех товаров равна: {p_summ}')
+# print(f'Налог с суммы равен: {tax:.2f}')
+# print(f'Итого за покупку: {total_summ}')

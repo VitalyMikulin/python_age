a792183db152fe9cfde88959128c66af038d08cc
Malkovich <malkovich@test.ru>
2024-09-03 22:10:18 +0400

    6

--- Chapter 2/exercises.py
+++ Chapter 2/exercises.py
@@ -21,3 +21,23 @@
 # print(f'Сумма всех товаров равна: {p_summ}')
 # print(f'Налог с суммы равен: {tax:.2f}')
 # print(f'Итого за покупку: {total_summ}')
+
+# №5
+# AUTO_SPEED = 70
+# print(f'Расстояние за 6 часов составляет: {AUTO_SPEED * 6} км')
+# print(f'Расстояние за 10 часов составляет: {AUTO_SPEED * 10} км')
+# print(f'Расстояние за 15 часов состоавляет: {AUTO_SPEED * 15} км')
+
+# №6
+
+# buy_summ = float(input('Введите общую сумму покупки: '))
+# federal_tax = buy_summ * 0.05
+# region_tax = buy_summ * 0.025
+# overall_tax = federal_tax + region_tax
+# final_summ = buy_summ + overall_tax
+# print(f'Сумма покупки составляет: {buy_summ:.2f}')
+# print(f'Федеральный налог с продаж составляет: {federal_tax:.2f}')
+# print(f'Региональный налог с продаж составляет: {region_tax:.2f}')
+# print(f'Общий налог с продаж составляет: {overall_tax:.2f}')
+# print(f'Общая сумма покупки составляет: {final_summ:.2f}')
+

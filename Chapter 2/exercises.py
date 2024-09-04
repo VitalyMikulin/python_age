8b41a5cc54ce3aaaa519f321335c5074b1d0ea9b
Malkovich <malkovich@test.ru>
2024-09-04 22:32:05 +0400

    12

--- Chapter 2/exercises.py
+++ Chapter 2/exercises.py
@@ -41,3 +41,55 @@
 # print(f'Общий налог с продаж составляет: {overall_tax:.2f}')
 # print(f'Общая сумма покупки составляет: {final_summ:.2f}')
 
+# №8
+
+# summ = float(input('Введите стоимость всей еды: '))
+# tips = summ * 0.18
+# sell_tax = summ * 0.07
+# total = summ + tips + sell_tax
+# print(f'Стоимость еды равна: {summ:.2f}')
+# print(f'Чаевые составляют: {tips:.2f}')
+# print(f'Налог с продаж составляет: {sell_tax:.2f}')
+# print(f'Общая сумма равна: {total:.2f}')
+
+# №9
+
+# celsium = float(input('Введите температуру в цельсиях: '))
+# farengheit = ((9 * celsium) / 5) + 32
+# print(f'Температура по фаренгейту равна: {farengheit:.2f}')
+
+# №10
+
+# one_cake_sugar = 1.5 / 48
+# one_cake_oil = 1 / 48
+# one_cake_flour = 2.75 / 48
+# cake_number = float(input('Введите желаемое количество булочек: '))
+# print(f'Для этого количества булочек необходимо: \n'
+#       f'{one_cake_sugar * cake_number:.1f} стакана сахара \n'
+#       f'{one_cake_oil * cake_number:.1f} стакана масла \n'
+#       f'{one_cake_flour * cake_number:.1f} стакана муки')
+
+# №11
+
+# male = float(input('Введите количество мальчиков: '))
+# female = float(input('Введите количество девочек: '))
+# male_percent = (male / (male + female))
+# female_percent = (female / (male + female))
+# print(f'Процент мальчиков равен: {male_percent:.0%}')
+# print(f'Процент девочек равен: {female_percent:.0%}')
+
+# №12
+
+# akcii = 2000
+# price_buy = 40.0
+# tax_buy = (akcii * price_buy) * 0.03
+# price_cell = 42.75
+# buy_summ = akcii * price_buy
+# cell_summ = akcii * price_cell
+# tax_cell = cell_summ * 0.03
+# final_total = cell_summ - buy_summ - (tax_buy + tax_cell) 
+# print(f'Акции куплены за {buy_summ:.2f} рублей')
+# print(f'Комиссия покупки равна {tax_buy:.2f} рублей')
+# print(f'Акции проданы за {cell_summ:.2f} рублей')
+# print(f'Комиссия продажи равна {tax_cell:.2f} рублей')
+# print(f'Итоговый профит: {final_total:.2f} рублей')

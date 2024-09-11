a2a86e7f81cf3caa36f6f7914900453c2f046da1
Malkovich <malkovich@test.ru>
2024-09-11 20:52:32 +0400

    543

--- Chapter 3/if.py
+++ Chapter 3/if.py
@@ -1,9 +1,15 @@
-HIGH_SCORE = 95
-test1 = int(input('Введите оценку 1: '))
-test2 = int(input('Введите оценку 2: '))
-test3 = int(input('Введите оценку 3: '))
-average = (test1 + test2 + test3) / 3
-print(f'Средний балл составляет: {average:.0f}')
-if average >= HIGH_SCORE:
-    print('Поздравляем!')
-    print('Отличный средний балл')
+# HIGH_SCORE = 95
+# test1 = int(input('Введите оценку 1: '))
+# test2 = int(input('Введите оценку 2: '))
+# test3 = int(input('Введите оценку 3: '))
+# average = (test1 + test2 + test3) / 3
+# print(f'Средний балл составляет: {average:.0f}')
+# if average >= HIGH_SCORE:
+#     print('Поздравляем!')
+#     print('Отличный средний балл')
+
+
+y = int(input('Введите значение для y: '))
+if y >= 10000:
+    x = 0.2
+print(x)

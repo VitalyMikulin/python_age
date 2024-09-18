a4af5307a9e20d68e7cdbf62afaa837ad646c4e4
Malkovich <malkovich@test.ru>
2024-09-18 20:37:14 +0400

    34

--- Chapter 3/if.py
+++ Chapter 3/if.py
@@ -9,7 +9,22 @@
 #     print('Отличный средний балл')
 
 
-y = int(input('Введите значение для y: '))
-if y >= 10000:
-    x = 0.2
-print(x)
+# y = int(input('Введите значение для y: '))
+# if y >= 10000:
+#     x = 0.2
+# else:
+#     x = 'фак ю'
+# print(x)
+
+BASE_HOURS = 40
+OT_MULTIPLIER = 1.5
+
+hours = float(input('Введите отработанное количество часов в неделю: '))
+pay_rate = float(input('Введите почасовую ставку: '))
+if hours > BASE_HOURS:
+    overtime_hours = hours - BASE_HOURS
+    overtime_pay = overtime_hours * pay_rate * OT_MULTIPLIER
+    gross_pay = BASE_HOURS * hours + overtime_pay
+else:
+    gross_pay = BASE_HOURS * hours
+print(f'Зарплата до удержания составляет: ${gross_pay:.2f}')

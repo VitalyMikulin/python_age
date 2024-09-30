0db1ff6d17cbff61a762224fe8507bf7537cd87b
Malkovich <malkovich@test.ru>
2024-09-30 21:52:04 +0400

    куук

--- Chapter 3/exercises.py
+++ Chapter 3/exercises.py
@@ -17,4 +17,70 @@
 # elif day == 7:
 #     print('Воскресенье')
 # else:
-#     print('Вы ввели неверное число.')
+#     print('Вы ввели неверное число.') 
+
+# №2
+# side_A_sq_1 = float(input('Введите сторону A прямоугольника 1: '))
+# side_B_sq_1 = float(input('Введите сторону B прямоугольника 1: '))
+# side_A_sq_2 = float(input('Введите сторону A прямоугольника 2: '))
+# side_B_sq_2 = float(input('Введите сторону B прямоугольника 2: '))
+
+# summ_1 = side_A_sq_1 * side_B_sq_1
+# summ_2 = side_A_sq_2 * side_B_sq_2
+
+# if summ_1 > summ_2:
+#     print('Площадь прямоугольника 1 больше площади прямоугольника 2')
+# elif summ_1 < summ_2:
+#     print('Площадь прямоугольника 2 больше площади прямоугольника 1')
+# else:
+#     print('Площади прямоугольников равны')
+
+# №3
+
+# age = float(input('Введите возраст человека: '))
+# if age <= 1:
+#     print('Это младенец.')
+# elif age > 1 and age < 13:
+#     print('Это ребёнок.')
+# elif age >= 13 and age < 20:
+#     print('Это подросток.')
+# else:
+#     print('Это взрослый.')
+
+# №4
+
+# number = int(input('Введите число от 1 до 10: '))
+# if number == 1:
+#     print('I')
+# elif number == 2:
+#     print('II')
+# elif number == 3:
+#     print('III')
+# elif number == 4:
+#     print('IV')
+# elif number == 5:
+#     print('V')
+# elif number == 6:
+#     print('VI')
+# elif number == 7:
+#     print('VII')
+# elif number == 8:
+#     print('VIII')
+# elif number == 9:
+#     print('IX')
+# elif number == 10:
+#     print('X')
+# else:
+#     print('Вы ввели не верные данные')
+
+# №5
+# first_color = input('Введите первый цвет: ')
+# second_color = input('Введите второй цвет: ')
+# if (first_color == 'синий' and second_color == 'красный') or (first_color == 'красный' and second_color == 'синий'):
+#     print('фиолетовый')
+# elif (first_color == 'красный' and second_color == 'желтый') or (first_color == 'желтый' and second_color == 'красный'):
+#     print('оранжевый')
+# elif (first_color == 'синий' and second_color == 'желтый') or (first_color == 'желтый' and second_color == 'синий'):
+#     print('зеленый')
+# else:
+#     print('вы ввели не правильные данные')

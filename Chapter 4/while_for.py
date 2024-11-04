c03c77b25fa4784469c413b7b5b280cc0a35267d
Malkovich <malkovich@test.ru>
2024-11-04 12:03:46 +0400

    545

---
+++ Chapter 4/while_for.py
@@ -0,0 +1,60 @@
+# keep_going = 'д'
+# while keep_going == 'д':
+#     sales = float(input('Введите объём продаж: '))
+#     comm_rate = float(input('Введите ставку комиссионных: '))
+#     commission = sales * comm_rate
+#     print(f'Комиссионное вознаграждение составляет ${commission:,.2f}.')
+#     keep_going = input('Хотите вычислить еще одно ' + 
+#                        '(Введите "д", если да): ')
+    
+# MAX_TEMP = 102.5
+# temperature = float(input('Введите температуру вещества в градусах Цельсия: '))
+# while temperature > MAX_TEMP:
+#     print('Температура слишком высокая')
+#     print('Убавьте нагрев и подождите')
+#     print('5 минут. Затем снимите показание температуры')
+#     print('снова и введите полученное значение.')
+#     temperature = float(input('Введите новое показание температуры: '))
+# print('Температура приемлемая')
+# print('Проверьте снова показание температуры через 15 минут')
+
+# print('Я покажу числа от 1 до 5')
+# for num in [1, 2, 3, 4, 5]:
+#     print(num)
+
+
+# for name in ['Мигнуть', 'Моргнуть', 'Кивнуть']:
+#     print(name)
+
+# print('Я покажу числа от 1 до 5')
+# for num in range(5):
+#     print(num)
+
+# for x in range(5):
+#     print('Привет, Мир!')
+
+
+# print('Я покажу числа от 1 до 5')
+# for num in range(1, 6):
+#     print(num)
+
+# for num in range(1, 10, 2):
+#     print(num)
+
+# print('Число\tКвадрат числа')
+# print('-----------------------')
+# for number in range(1, 11):
+#     square = number**2
+#     print(f'{number}\t{square}')
+
+# START_SPEED = 60
+# END_SPEED = 131
+# INCREMENT = 10
+# CONVERSATION_FACTOR = 0.6214
+# print('\n')
+# print('KPH\tMPH')
+# print('--------------------------------')
+# for kph in range(START_SPEED, END_SPEED, INCREMENT):
+#     mph = kph * CONVERSATION_FACTOR
+#     print(f'{kph}\t{mph:.1f}')
+

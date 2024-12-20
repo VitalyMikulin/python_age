3c921426825f14892f4f0432f945dce419a49121
Malkovich <malkovich@test.ru>
2024-12-20 19:48:10 +0400

    79679

---
+++ Chapter 5/func.py
@@ -0,0 +1,17 @@
+# def message():
+#     print('Я Артур,')
+#     print('король британцев')
+
+# message()
+
+
+def main():
+    print('У меня для вас известие.')
+    message()
+    print('До свидания!')
+
+def message():
+    print('Я Артур,')
+    print('король британцев.')
+
+main()

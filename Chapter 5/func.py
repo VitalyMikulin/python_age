d82553c1269a91a2062f1bced720e78ca68b410c
Malkovich <malkovich@test.ru>
2025-01-15 21:50:28 +0400

    789

--- Chapter 5/func.py
+++ Chapter 5/func.py
@@ -16,8 +16,34 @@
 
 # main()
 
-def show_double(number):
-    result = number * 2
-    print(result)
+# def show_double(number):
+#     result = number * 2
+#     print(result)
 
-show_double(5.3)
+# show_double(5.3)
+
+# def main():
+#     intro()
+#     cups_needed = int(input('Введите число чашек: '))
+#     cups_to_ounces(cups_needed)
+
+# def intro():
+#     print('Бла бла бла')
+#     print()
+
+# def cups_to_ounces(cups):
+#     ounces = cups * 8
+#     print(f'Это число конвертируется в {ounces} унций')
+
+# main()
+
+# def main():
+#     num1 = int(input('Введите первое число: '))
+#     num2 = int(input('Введите второе число: '))
+#     sum(num1, num2)
+
+# def sum(digit1, digit2):
+#     summa = digit1 + digit2
+#     print(f'Сумма этих двух чисел равняется {summa}')
+
+# main()

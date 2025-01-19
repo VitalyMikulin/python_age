e2f02cfd94140818cf08344eb7e834a83beba08e
Malkovich <malkovich@test.ru>
2025-01-19 18:45:46 +0400

    867

--- Chapter 5/func.py
+++ Chapter 5/func.py
@@ -47,3 +47,56 @@
 #     print(f'Сумма этих двух чисел равняется {summa}')
 
 # main()
+
+
+# number = 0
+
+# def main():
+#     global number
+#     number = int(input('Введите число: '))
+#     show_number()
+
+# def show_number():
+#     print(f'Вы ввели число {number}')
+
+# main()
+
+# CONTRIBUTION_RATE = 0.05
+
+# def main():
+#     gross_pay = float(input('Введите заработную плату: '))
+#     bonus = float(input('Введите сумму премий: '))
+#     show_pay_contrib(gross_pay)
+#     show_bonus_contrib(bonus)
+
+# def show_pay_contrib(gross):
+#     contrib = gross * CONTRIBUTION_RATE
+#     print(f'Взнос исходя из заработной платы ${contrib:,.2f}')
+
+# def show_bonus_contrib(bonus):
+#     contrib = bonus * CONTRIBUTION_RATE
+#     print(f'Взнос исходя из суммы пермий: ${contrib:,.2f}')
+
+# main()
+
+import random
+
+# def main():
+#     for count in range(5):
+#         print(f'Число равняется {random.randint(1, 100):^10d}')
+
+# main()
+
+# random.randint(1, 10)
+
+MIN = 1
+MAX = 6
+
+def main():
+    again = 'y'
+
+    while again == 'y' or again == 'Y':
+        print('Выпали следующие кубики: ')
+        print(random.randint(MIN, MAX), random.randint(MIN, MAX))
+        again = input('Сделать еще один бросок (y/n)?: ')
+main()

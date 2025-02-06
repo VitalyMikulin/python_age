d892218ac061f578c7fcfbdbfb3b7fca135ac98e
Malkovich <malkovich@test.ru>
2025-02-06 21:55:18 +0400

    565н

--- Chapter 5/func.py
+++ Chapter 5/func.py
@@ -175,3 +175,49 @@ import random
 # print(fama, imechko)
 
 
+import circle
+import rectangle
+
+AREA_CIRCLE_CHOICE = 1
+CIRCUMFERENCE_CHOICE = 2
+AREA_RECTANGLE_CHOICE = 3
+PERIMETER_RECTANGLE_CHOICE = 4
+QUIT_CHOICE = 5
+
+def main():
+    choice = 0
+
+    while choice != QUIT_CHOICE:
+        display_menu()
+
+        choice = int(input('Выберите вариант: '))
+
+        if choice == AREA_CIRCLE_CHOICE:
+            radius = float(input('Введите радиус: '))
+            print('Площадь курга равна', circle.area(radius))
+        elif choice == CIRCUMFERENCE_CHOICE:
+            radius = float(input('Введите радиус: '))
+            print('Длина окружности равна ',
+                  circle.circumference(radius))
+        elif choice == AREA_RECTANGLE_CHOICE:
+            width = float(input('Введите ширину: '))
+            length = float(input('Введите длину: '))
+            print('Площадь равна ', rectangle.area(width, length))
+        elif choice == PERIMETER_RECTANGLE_CHOICE:
+            width = float(input('Введите ширину: '))
+            length = float(input('Введите длину: '))
+            print('Периметр равен ', rectangle.perimeter(width, length))
+        elif choice == QUIT_CHOICE:
+            print('Выходим из программы...')
+        else:
+            print('Ошибка: недопустимый выбор.')
+
+def display_menu():
+    print('МЕНЮ')
+    print('1. Площадь круга')
+    print('2. Длина окружности')
+    print('3. Площадь прямоугольника')
+    print('4. Периметр прямоугольника')
+    print('5. Выход')
+
+main()

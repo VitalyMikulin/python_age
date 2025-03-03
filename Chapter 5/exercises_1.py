0599ec737b1537456f31fd288fb0795d58fe851a
Malkovich <malkovich@test.ru>
2025-03-03 21:45:57 +0400

    5345

---
+++ Chapter 5/exercises_1.py
@@ -0,0 +1,52 @@
+# def times_ten(number):
+#     print(number * 10)
+
+# x = int(input('Введите число: '))
+
+# times_ten(x)
+
+# def main():
+#     x = 1
+#     y = 3.4
+#     print(x, y)
+#     change_us(x, y)
+#     print(x, y)
+
+# def change_us(a, b):
+#     a = 0
+#     b = 0
+#     print(a, b)
+
+# main()
+
+# def my_function(a, b, c):
+#     d = (a + c) / b
+#     print(d)
+
+# my_function(2, 4, 6)
+
+# import random
+
+# rand = random.randint(1, 100)
+# print(rand)
+
+# def half(parameter):
+#     d = parameter / 2
+#     return d
+
+# number = float(input('Введите число: '))
+# f = half(number)
+# print(f)
+
+# def cube(num):
+#     return num * num * num
+
+# result = cube(4)
+# print(result)
+
+# def get_first_name():
+#     name = input('Введите ваше имя: ')
+#     return name
+
+# d = get_first_name()
+# print(d)
